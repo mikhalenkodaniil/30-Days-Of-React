ba951b92d6bae8c75bdd86841db2673f76e1be8c
mikhalenkodaniil <mikhalenko.daniil@gmail.com>
2023-09-11 13:32:21 +0200

    chore: exercise day5 complete

---
+++ solutions/day-05/level1/index.jsx
@@ -0,0 +1,14 @@
+// 1. What is props in a React component ?
+// props in react compponent == props in js functions
+
+// 2. How do you access props in a React component ?
+// call for name
+
+// 3. What data types can we pass as props to components ?
+// all js data types
+
+// 4. What is a propTypes?
+// The propTypes package helps us to assign the data types of the props we passed to a component.
+
+// 5. What is a default propTypes?
+// The defaultProps can be used when we want to have some default prop types for a component.
