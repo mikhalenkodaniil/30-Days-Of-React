35194cf0ecf36e0473386fdfa01c31594356015b
mikhalenkodaniil <mikhalenko.daniil@gmail.com>
2023-09-12 13:57:42 +0200

    chore: exercise day7 complete

---
+++ solutions/day-07/level1/index.jsx
@@ -0,0 +1,24 @@
+// 1. How do you write a pure JavaScript function
+// function functionName () {
+//     return null;
+// }
+// 2. What is inheritance and how do you make a child from a parent class?
+// class child extends parent () {
+//     constructor() {
+//         super();
+//     }
+// }
+// 3. What is class based React component ?
+// component, which based on classes
+// 4. What is the difference between functional React component and class based React component ?
+// in class based component data flow on parent to child component, but in fuctional component we can push data in props
+// 5. When do we need to use class based components instead of functional components
+// never. Its antipatern
+// 6. What is the use cases of class based component ?
+// different
+// 7. Which type of component do use most frequently ? functional or class-based component
+// functional
+// 8. What is React life cycle ? (not covered yet) ?
+// cycle, which change state in real time
+// 9. What is state in React ? (not covered yet)
+// current state of component
