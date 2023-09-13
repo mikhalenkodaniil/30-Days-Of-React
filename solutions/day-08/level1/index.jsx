896353ca5dba72e3c2c916098b71ca5e9c31f491
mikhalenkodaniil <mikhalenko.daniil@gmail.com>
2023-09-13 12:49:55 +0200

    chore: exercise day8 complete

---
+++ solutions/day-08/level1/index.jsx
@@ -0,0 +1,8 @@
+// 1. What was your state today? Are you happy? I hope so. If you manage to make it this far you should be happy.
+// 2. What is state in React ?
+// 3. What is the difference between props and state in React ?
+// 4. How do you access state in a React component ?
+// const { useState } = require("react");
+// useState setState
+// 5. How do you set a set in a React component ?
+// useState setState
