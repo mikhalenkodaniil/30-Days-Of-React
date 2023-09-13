8dbc35cac31f90afc883005c29d55f45e2c6cd1d
mikhalenkodaniil <mikhalenko.daniil@gmail.com>
2023-09-13 17:24:19 +0200

    chore: exercise day10 complete

---
+++ solutions/day-10/level1/index.jsx
@@ -0,0 +1,11 @@
+// 1. What is the importance of React Folder Structure and File Naming
+// good names, which understand team
+// 2. How do you export file
+// export functiocn fName(){}
+// 3. How do you import file
+// import fname frome "./FName.js"
+// 4. Make a component of module and export it as named or default export
+// 
+// 5. Make a component or module and import it
+//
+// 6. Change all the components you have to different folder structure
