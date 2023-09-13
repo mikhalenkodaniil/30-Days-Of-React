d757962469646fe86a26fc2e67054c30fad2c4d5
mikhalenkodaniil <mikhalenko.daniil@gmail.com>
2023-09-13 17:12:13 +0200

    chore: exercise day9 complete

---
+++ solutions/day-09/level1/index.jsx
@@ -0,0 +1,6 @@
+// 1. What is conditional rendering?
+// when you rendering page, which render depends to conditions
+// 2. How do you implement conditional rendering?
+// whith:  ?? / && / if else
+// 3. Which method of conditional rendering do you prefer to use?
+// &&
