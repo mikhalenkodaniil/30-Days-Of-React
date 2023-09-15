899e3e1f26c209c18dc7b8cc65266321af197201
mikhalenkodaniil <mikhalenko.daniil@gmail.com>
2023-09-15 11:08:34 +0200

    chore: exercise day12 complete

---
+++ solutions/day-12/level1/index.js
@@ -0,0 +1,65 @@
+// 1. What is the importance of form?
+{/* <form>
+    <label></label>
+    <input></input>
+</form>
+
+<select>
+    <option></option>
+    <option></option>
+    <option></option>
+</select> */}
+// 2. How many input types do you know?
+{/* <input type="text" />
+<input type="number" />
+<input type="range" />
+
+<input type="email" />
+<input type="password" />
+<input type="tel" />
+
+<input type="checkbox" />
+<input type="radio" />
+
+<input type="color" />
+
+<input type="url" />
+<input type="image" />
+<input type="file" />
+
+<input type="hidden" />
+
+<input type="date" />
+<input type="datetime-local" />
+<input type="month" />
+<input type="week" />
+<input type="time" />
+
+<input type="reset" />
+<input type="search" />
+<input type="submit" />
+<input type="button" /> */}
+
+// 3. Mention at least four attributes of an input element
+// type=""
+// placeholder=""
+// src=""
+// value=""
+
+// 4. What is the importance of htmlFor?
+// When used together with the <label> element, the for attribute specifies which form element a label is bound to.
+// When used together with the <output> element,
+// the for attribute specifies the relationship between the result of the calculation,
+// and the elements used in the calculation.
+
+// 5. Write an input type which is not given in the example if there is?
+// 6. What is a controlled input?
+// 7. What do you need to write a controlled input?
+// 8. What event type do you use to listen changes on an input field?
+// 9. What is the value of a checked checkbox?
+// 10. When do you use onChange, onBlur, onSubmit?
+// 11. What is the purpose of writing e.preventDefault() inside the submit handler method?
+// 12. How do you bind data in React? The first input field example is data binding in React.
+// 13. What is validation?
+// 14. What is the event type you use to listen when an input changes?
+// 15. What are event types do you use to validate an input?
