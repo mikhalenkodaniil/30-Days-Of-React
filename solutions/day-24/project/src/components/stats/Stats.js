802fb51c8753a27368c0439f1f3aa597398d75ac
mikhalenkodaniil <mikhalenko.daniil@gmail.com>
2023-09-20 13:35:21 +0200

    chore: exercise day24 complete main page section

--- solutions/day-24/project/src/components/stats/Stats.js
+++ solutions/day-24/project/src/components/stats/Stats.js
@@ -1,17 +1,31 @@
-// import PopulationStats from "../populationStats/PopulationStats";
+import PopulationStats from "../populationStats/PopulationStats";
 import LanguageStats from "../languageStats/LanguageStats";
 import "./stats.css";
+import { useState } from "react";
 
 export default function Stats({ data }) {
-    // const statsOut = <PopulationStats data={data} />
+    let statsOut;
+    const [flag, setFlag] = useState(true);
+    if (data[0]) {
+        if (flag) {
+            statsOut = <PopulationStats cdata={data} />;
+        } else {
+            statsOut = <LanguageStats data={data} />;
+        }
+    }
+     function setTrue() {
+        setFlag(true);
+     }
+     function setFalse() {
+        setFlag(false);
+     }
   return (
     <section className="stats" id="stats">
       <div>
-        <button className="stats__btn">POPULATION</button>
-        <button className="stats__btn">LANGUAGES</button>
+        <button className="stats__btn" onClick={setTrue}>POPULATION</button>
+        <button className="stats__btn" onClick={setFalse}>LANGUAGES</button>
       </div>
-      {/* {statsOut} */}
-      <LanguageStats data={data} />
+      {statsOut}
     </section>
   );
 }
