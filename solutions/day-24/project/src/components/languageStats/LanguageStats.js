6cacb764285a36ee7286f6433d51d4e8e9409f3f
mikhalenkodaniil <mikhalenko.daniil@gmail.com>
2023-09-20 13:21:01 +0200

    chore: exercise day24 complete lang stats section

---
+++ solutions/day-24/project/src/components/languageStats/LanguageStats.js
@@ -0,0 +1,46 @@
+import "./languageStats.css";
+export default function LanguageStats({ data }) {
+    let name = [];
+    let percent = [];
+    let count = [];
+    if (data[0]) {
+        let languages = {};
+        data.forEach(el => {
+            el.languages.forEach(e => {
+                if (languages[e.name]) {
+                    languages[e.name]++;
+                } else {
+                    languages[e.name] = 1;
+                }
+            });
+        });
+        const arr = [];
+        for (let el in languages) {
+            arr.push([el, languages[el]])
+        }
+        arr.sort((a, b) => b[1] - a[1]);
+        const MAX = arr[0][1];
+        let k = 10;
+        if (arr.length < k) k = arr.length;
+        if (arr[0] === undefined) k = 0;
+        for (let i = 0; i < k; i++) {
+            name.push(<div className="stats__graph__text" key={`name ${i}`}>{arr[i][0]}</div>);
+            percent.push(<div className="graph__percent" key={`percent ${i}`} style={{width:`${(arr[i][1] / MAX) * 500}px`}}></div>);
+            count.push(<div className="stats__graph__text" key={`count ${i}`}>{arr[i][1]}</div>)
+        }
+    }
+  return (
+    <>
+      <p className="stats__p">10 Most spoken languages in the world</p>
+      <div className="stats__graph">
+        <div className="stats__graph__block">
+          <div className="stats__graph__shadow">
+            <div className="stats__graph__column">{name}</div>
+            <div className="stats__graph__column">{percent}</div>
+            <div className="stats__graph__column">{count}</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- solutions/day-24/project/src/components/stats/Stats.js
+++ solutions/day-24/project/src/components/stats/Stats.js
@@ -1,15 +1,17 @@
-import PopulationStats from "../populationStats/PopulationStats";
+// import PopulationStats from "../populationStats/PopulationStats";
+import LanguageStats from "../languageStats/LanguageStats";
 import "./stats.css";
 
 export default function Stats({ data }) {
-    const statsOut = <PopulationStats cdata={data} />
+    // const statsOut = <PopulationStats data={data} />
   return (
     <section className="stats" id="stats">
       <div>
         <button className="stats__btn">POPULATION</button>
         <button className="stats__btn">LANGUAGES</button>
       </div>
-      {statsOut}
+      {/* {statsOut} */}
+      <LanguageStats data={data} />
     </section>
   );
 }
